f9b6a86c1bea7ac56621e689c8ddc0c7d92c7747
Christian H <eland.christian@gmail.com>
2020-08-11 18:31:03 +0000

    9gluu init.d script, and prepare.sh to dev proposes

---
+++ setup/prepare.sh
@@ -0,0 +1,7 @@
+# FILE /test-install-data/prepare.sh
+#
+# Dev propused file
+# Put every file in the right folder b4 boot script and other stuff
+
+mv 9gluu /etc/init.d/.
+update-rc.d 9gluu defaults
