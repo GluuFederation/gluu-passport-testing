4ba8af312624e958e767dab5948d73ae06c118e0
Christian H <eland.christian@gmail.com>
2020-09-17 18:01:17 +0000

    deleted old unused helpers

--- tests/helpers/oidc_helper.py
+++
@@ -1,9 +0,0 @@
-from ldaphelper import Op, LdapConnector
-import json
-from flask_oidc import registration, discovery
-import os
-
-
-disc = discovery.discover_OP_information('https://chris.gluutwo.org')
-
-reg = registration.register_client(disc,['https://chris.gluuthree.org/callback'])
