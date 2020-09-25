eb918e40e0db2f83a28a7c3c17befb11fed11a5f
Christian H <eland.christian@gmail.com>
2020-09-25 00:12:27 +0000

    feat: allow to setup gluu.sh with dev / stable versions

--- setup/gluu.sh
+++ setup/gluu.sh
@@ -5,10 +5,14 @@
 if [ -f /root/freshsnap ]
 then
     ### Ads and install package for ubuntu 18
-    echo "deb https://repo.gluu.org/ubuntu/ bionic main" > /etc/apt/sources.list.d/gluu-repo.list
+    ### Uncomment bellow for latest stable
+    # echo "deb https://repo.gluu.org/ubuntu/ bionic main" > /etc/apt/sources.list.d/gluu-repo.list
+    ### Uncomment bellow for latest dev
+    echo "deb https://repo.gluu.org/ubuntu/ bionic-devel main" > /etc/apt/sources.list.d/gluu-repo.list
     curl https://repo.gluu.org/ubuntu/gluu-apt.key | apt-key add -
     apt update -y
-    apt install gluu-server=4.2.0~bionic -y
+    ### Install latest stable or dev
+    apt install gluu-server -y
     apt-mark hold gluu-server -y
     ### copy setup files to chmod
     echo "Copying setup files to chmod..."
@@ -21,25 +25,25 @@ then
     echo "Copying certbot script to chmod..."
     ### Certbot to root chmod
     cp /test-install-data/certbot.sh /opt/gluu-server/root/.
-
+    
     echo "Creating flag file on chroot to initiate script on first login to chroot..."
     ### create flag file on chroot
     touch /opt/gluu-server/root/fresh
-
+    
     echo "Copying script to setup gluu-server on first login to chroot..."
     ### copy install script to profile
     cp /test-install-data/setupscript.sh /opt/gluu-server/etc/profile.d/.
-
+    
     echo "Extracting letsencrypt files to chmod..."
     ## extract certificates to fresh install
     tar -zxvf /test-install-data/etcletsencrypt.tar -C /opt/gluu-server
-
+    
     # Enable, start, login gluu
     gluu-serverd enable
     echo "Starting gluu-serverd..."
     gluu-serverd start
     echo "Loggin in to execute next script..."
     gluu-serverd login
-
+    
     rm -f /root/freshsnap
 fi
