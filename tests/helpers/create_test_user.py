0afaabe22b8f1e52f3754b76491f4d9fe014a8d6
Christian H <eland.christian@gmail.com>
2020-10-04 02:46:44 +0000

    style(user-creation): Fix snake_case and db name

--- tests/helpers/create_test_user.py
+++ tests/helpers/create_test_user.py
@@ -2,7 +2,6 @@ from users_helper import TestUser
 
 # CREATES USER
 
-
 test_user = TestUser()
 
 token = test_user.get_token()
@@ -10,5 +9,3 @@ print("Token generated: %s" % token)
 
 
 created_user = test_user.create_user(token)
-
-
