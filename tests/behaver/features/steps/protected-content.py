f49a9a860b00effbd18ba82a5ce01d3cbdd6a505
christian-hawk <eland.christian@gmail.com>
2020-10-20 22:07:37 +0000

    feat(destroy_droplet.py): destroy droplet after test
    env.sh calls destroy_droplet.py
    
    closes 11

--- tests/behaver/features/steps/protected-content.py
+++ tests/behaver/features/steps/protected-content.py
@@ -9,27 +9,29 @@ from datetime import datetime
 # in case of self-signed
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def cookiesTransformer(sel_session_id,sel_other_cookies):
+
+def cookiesTransformer(sel_session_id, sel_other_cookies):
     ''' This transform cookies from selenium to requests '''
     s = requests.Session()
-    s.cookies.set('session_id',sel_session_id)
+    s.cookies.set('session_id', sel_session_id)
     i = 0
     while i < len(sel_other_cookies):
         s.cookies.set(
             sel_other_cookies[i]['name'],
             sel_other_cookies[i]['value'],
-            path = sel_other_cookies[i]['path'],
-            domain = sel_other_cookies[i]['domain'],
-            secure = sel_other_cookies[i]['secure'],
-            rest= {'httpOnly' : sel_other_cookies[i]['httpOnly']}
-            )
+            path=sel_other_cookies[i]['path'],
+            domain=sel_other_cookies[i]['domain'],
+            secure=sel_other_cookies[i]['secure'],
+            rest={'httpOnly': sel_other_cookies[i]['httpOnly']}
+        )
         i = i+1
 
     return s
 
+
 def getIdpInitiatedLink(context):
     return 'https://%s/idp/profile/SAML2/Unsolicited/SSO?providerId=%s&target=%s/protected-content' % (
-            context.provider_host, context.client_host, context.base_url )
+        context.provider_host, context.client_host, context.base_url)
 
 
 @given(u'that user exists in provider')
@@ -46,57 +48,63 @@ def user_authenticates(context):
 
     time.sleep(3)
 
-
     if context.acr == "passport-saml":
 
         # for all samls..
         time.sleep(2)
-        context.web.save_screenshot(str(datetime.now())+"3_before_username_typing.png")
+        context.web.save_screenshot(
+            str(datetime.now())+"3_before_username_typing.png")
         context.web.find_element(By.ID, "username").click()
-        context.web.find_element(By.ID, "username").send_keys(context.user_name)
-        context.web.find_element(By.ID, "password").send_keys(context.user_password)
-        context.web.save_screenshot(str(datetime.now())+"4_before_login_button.png")
+        context.web.find_element(
+            By.ID, "username").send_keys(context.user_name)
+        context.web.find_element(By.ID, "password").send_keys(
+            context.user_password)
+        context.web.save_screenshot(
+            str(datetime.now())+"4_before_login_button.png")
         context.web.find_element(By.ID, "loginButton").click()
 
         time.sleep(3)
-        context.web.save_screenshot(str(datetime.now())+"5_after_login_button.png")
-
+        context.web.save_screenshot(
+            str(datetime.now())+"5_after_login_button.png")
 
         try:
-            context.web.find_element(By.ID, "authorizeForm:allowButton").click()
+            context.web.find_element(
+                By.ID, "authorizeForm:allowButton").click()
         except NoSuchElementException:
             # Couldn't find consent form, presuming user already consented...
             pass
 
         time.sleep(2)
 
-
         if context.flow == "default emailreq":
             # should ask for e-mail input
-            context.web.save_screenshot(str(datetime.now())+"6_before_enter_email.png")
+            context.web.save_screenshot(
+                str(datetime.now())+"6_before_enter_email.png")
             time.sleep(5)
 
             try:
-                context.web.find_element(By.ID, "loginForm:email").send_keys(context.user_mail)
-                context.web.save_screenshot(str(datetime.now())+"7_after_enter_email.png")
+                context.web.find_element(
+                    By.ID, "loginForm:email").send_keys(context.user_mail)
+                context.web.save_screenshot(
+                    str(datetime.now())+"7_after_enter_email.png")
                 context.web.find_element(By.ID, "loginForm:j_idt14").click()
                 time.sleep(2)
-                context.web.save_screenshot(str(datetime.now())+"8_after_click_login_button.png")
+                context.web.save_screenshot(
+                    str(datetime.now())+"8_after_click_login_button.png")
             except NoSuchElementException:
                 # Couldn't find email form, presuming user formerly entered...
                 pass
             try:
-                context.web.find_element(By.ID, "authorizeForm:allowButton").click()
+                context.web.find_element(
+                    By.ID, "authorizeForm:allowButton").click()
             except NoSuchElementException:
                 pass
                 # "Couldn't find consent form, presuming user already consented...")
 
         time.sleep(2)
 
-
         time.sleep(2)
         # print(context.web.current_url)
-
 
     if context.acr == "oidc":
 
@@ -105,10 +113,10 @@ def user_authenticates(context):
         context.web.find_element(By.ID, "username").click()
         context.web.find_element(By.ID, "username").send_keys(context.username)
         time.sleep(2)
-        context.web.find_element(By.ID, "password").send_keys(context.user_password)
+        context.web.find_element(By.ID, "password").send_keys(
+            context.user_password)
         context.web.find_element(By.ID, "loginButton").click()
         time.sleep(2)
-
 
     assert context.web.current_url.startswith(context.base_url)
     context.web.save_screenshot(str(datetime.now())+"_authenticated.png")
@@ -123,7 +131,8 @@ def user_clicks_protected_content_link(context):
     else:
         context.web.get(context.base_url)
         time.sleep(2)
-        context.web.find_element_by_xpath('//a[@href="'+context.base_url+"/protected-content"+'"]').click()
+        context.web.find_element_by_xpath(
+            '//a[@href="'+context.base_url+"/protected-content"+'"]').click()
         context.has_clicked = True
     time.sleep(2)
 
@@ -147,13 +156,12 @@ def user_redirected_to_external_login_page(context):
     assert selenium_url.startswith(external_login_page)
 
 
-
-
 @then(u'user should access protected content')
 def user_access_protected_content(context):
     # We fetch cookies from selenium and pass them through request to validate
-    new_sess = cookiesTransformer(context.web.session_id,context.web.get_cookies())
-    res = new_sess.get(context.base_url + "/protected-content",verify = False)
+    new_sess = cookiesTransformer(
+        context.web.session_id, context.web.get_cookies())
+    res = new_sess.get(context.base_url + "/protected-content", verify=False)
 
     assert res.url == context.base_url + "/protected-content"
     context.web.save_screenshot(str(datetime.now())+"_protected-content.png")
---
+++ tests/helpers/destroy_droplet.py
@@ -0,0 +1,20 @@
+import ipdb
+from pydoautomator import Automator
+import os
+
+digital_ocean_token = os.getenv("DO_TOKEN")
+
+# requires ENV
+name = os.getenv('DROPLET_HOST')
+floating = os.getenv('FLOATING_IP')
+snapshot = os.getenv('SNAPSHOT_ID')
+
+
+aut = Automator(digital_ocean_token)
+
+droplets = aut.get_all_droplets()
+
+for droplet in droplets:
+    if droplet['name'] == name:
+        aut.turnoff_droplet(droplet['id'])
+        aut.destroy_droplet(droplet['id'])
