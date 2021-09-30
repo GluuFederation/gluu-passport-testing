2232d258d3259931e00d81e8c918c620f40c39d2
kdhttps <kdhttps@gmail.com>
2021-09-30 18:33:36 +0530

    ci(install_and_setup.sh): update paths in setup.properties

--- saml_setup/install_and_setup.sh
+++ saml_setup/install_and_setup.sh
@@ -19,11 +19,11 @@ gluu-serverd login
 echo "All finished!"
 
 ### Move preconfigured setup.properties to gluu setup directory
-sed -i "1s/.*/ip=$PASSPORT_HOST_IP/" /test-install-data/setup.properties
-sed -i "2s/.*/ldapPass=$PASSPORT_HOST_GLUU_ADMIN_PASSWORD/" /test-install-data/setup.properties
-sed -i "3s/.*/oxtrust_admin_password=$PASSPORT_HOST_GLUU_ADMIN_PASSWORD/" /test-install-data/setup.properties
+sed -i "1s/.*/ip=$PASSPORT_HOST_IP/" ~/gluu-passport-testing/setup.properties
+sed -i "2s/.*/ldapPass=$PASSPORT_HOST_GLUU_ADMIN_PASSWORD/" ~/gluu-passport-testing/setup.properties
+sed -i "3s/.*/oxtrust_admin_password=$PASSPORT_HOST_GLUU_ADMIN_PASSWORD/" ~/gluu-passport-testing/setup.properties
 
-cp /test-install-data/setup.properties /opt/gluu-server/install/community-edition-setup
+cp ~/gluu-passport-testing/setup.properties /opt/gluu-server/install/community-edition-setup
 
 ### Setup Gluu Server
 gluu-serverd login << EOF
