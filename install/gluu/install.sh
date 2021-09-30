ffb70991e407b798497a7d06b44af7470f190008
kdhttps <kdhttps@gmail.com>
2021-09-30 19:50:17 +0530

    ci(install.sh): remove unwanted gluu login

--- install/gluu/install.sh
+++ install/gluu/install.sh
@@ -5,6 +5,7 @@
 ### Need two environment PASSPORT_HOST PASSPORT_HOST_IP PASSPORT_HOST_GLUU_ADMIN_PASSWORD
 
 ### Ads and install latest STABLE package for ubuntu 18
+echo "Installing Gluu..."
 echo "deb https://repo.gluu.org/ubuntu/ focal main" > /etc/apt/sources.list.d/gluu-repo.list
 curl https://repo.gluu.org/ubuntu/gluu-apt.key | apt-key add -
 apt update -y
@@ -12,13 +13,11 @@ apt install gluu-server -y
 apt-mark hold gluu-server -y
 
 gluu-serverd enable
-echo "Starting gluu-serverd..."
+echo "Starting Gluu..."
 gluu-serverd start
-echo "Loggin in to execute next script..."
-gluu-serverd login
-echo "All finished!"
 
 ### Move preconfigured setup.properties to gluu setup directory
+echo "Making Config File..."
 test_data_dir=/root/gluu-passport-testing/install
 setup_property_file=$test_data_dir/gluu/templates/setup.properties 
 sed -i "1s/.*/ip=$PASSPORT_HOST_IP/" $setup_property_file
@@ -29,6 +28,7 @@ sed -i "4s/.*/hostname=$PASSPORT_HOST/" $setup_property_file
 cp $setup_property_file /opt/gluu-server/install/community-edition-setup
 
 ### Setup Gluu Server
+echo "Setuping Gluu..."
 gluu-serverd login << EOF
 cd /install/community-edition-setup/
 ./setup.py -n -c
